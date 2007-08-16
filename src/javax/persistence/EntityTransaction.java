6d9f88ef64b9bd01e2de5e29aa301a2fb52519f0
Emmanuel Bernard <emmanuel@hibernate.org>
2007-08-16 00:17:27 +0000

    Move jpa-api tags/annotations_v31beta9 to the new structure (annotations_v3_1_0_Beta9)
    git-svn-id: http://anonsvn.jboss.org/repos/hibernate/jpa-api/tags/annotations_v3_1_0_Beta9@13239 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ src/javax/persistence/EntityTransaction.java
@@ -0,0 +1,53 @@
+//$Id$
+package javax.persistence;
+
+public interface EntityTransaction {
+	/**
+	 * Start a resource transaction.
+	 *
+	 * @throws IllegalStateException if isActive() is true.
+	 */
+	public void begin();
+
+	/**
+	 * Commit the current transaction, writing any unflushed
+	 * changes to the database.
+	 *
+	 * @throws IllegalStateException if isActive() is false.
+	 * @throws RollbackException	 if the commit fails.
+	 */
+	public void commit();
+
+	/**
+	 * Roll back the current transaction.
+	 *
+	 * @throws IllegalStateException if isActive() is false.
+	 * @throws PersistenceException  if an unexpected error
+	 *                               condition is encountered.
+	 */
+	public void rollback();
+
+	/**
+	 * Mark the current transaction so that the only possible
+	 * outcome of the transaction is for the transaction to be
+	 * rolled back.
+	 *
+	 * @throws IllegalStateException if isActive() is false.
+	 */
+	public void setRollbackOnly();
+
+	/**
+	 * Determine whether the current transaction has been marked
+	 * for rollback.
+	 *
+	 * @throws IllegalStateException if isActive() is false.
+	 */
+	public boolean getRollbackOnly();
+
+	/**
+	 * Indicate whether a transaction is in progress.
+	 * @throws PersistenceException if an unexpected error
+	 * condition is encountered.
+	 */
+	public boolean isActive();
+}
---
+++ src/javax/persistence/LockModeType.java
@@ -0,0 +1,10 @@
+//$Id$
+package javax.persistence;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public enum LockModeType {
+	READ,
+	WRITE
+}
