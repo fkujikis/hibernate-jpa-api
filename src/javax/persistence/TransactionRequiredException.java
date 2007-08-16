cd1a8d776fbd4236e189c1fb4931951f1546f4d2
Emmanuel Bernard <emmanuel@hibernate.org>
2007-08-16 00:09:11 +0000

    Move jpa-api tags/annotations_v3_2_0_CR2 to the new structure (annotations_v3_2_0_CR2)
    git-svn-id: http://anonsvn.jboss.org/repos/hibernate/jpa-api/tags/annotations_v3_2_0_CR2@13159 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ src/javax/persistence/TransactionRequiredException.java
@@ -0,0 +1,26 @@
+//$Id$
+package javax.persistence;
+
+/**
+ * Thrown by the persistence provider when a transaction is required but is not active.
+ * @author Gavin King
+ */
+public class TransactionRequiredException extends PersistenceException {
+
+	/**
+	 * Constructs a new TransactionRequiredException exception with null as its detail message
+	 */
+	public TransactionRequiredException() {
+		super();
+	}
+
+	/**
+	 * Constructs a new TransactionRequiredException exception with the specified detail message
+	 * 
+	 * @param message
+	 */
+	public TransactionRequiredException(String message) {
+		super( message );
+	}
+
+}
---
+++ src/javax/persistence/UniqueConstraint.java
@@ -0,0 +1,22 @@
+//$Id$
+//EJB3 Specification Copyright 2004-2006 Sun Microsystems, Inc.
+package javax.persistence;
+
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to specify that a unique constraint is to be included in the generated DDL
+ * for a primary or secondary table
+ *
+ * @author Emmanuel Bernard
+ */
+@Target({TYPE}) @Retention(RUNTIME)
+public @interface UniqueConstraint {
+	/**
+	 * An array of the column names that make up the constraint
+	 */
+	String[] columnNames();
+}
