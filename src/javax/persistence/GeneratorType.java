8a7a5bea209c028dd363d10d9e38ad58a79d14d0
Emmanuel Bernard <emmanuel@hibernate.org>
2007-08-16 00:53:19 +0000

    Move jpa-api tags/entitymanager_v31beta2 to the new structure (entitymanager_v3_1_0_Beta2)
    git-svn-id: http://anonsvn.jboss.org/repos/hibernate/jpa-api/tags/entitymanager_v3_1_0_Beta2@13768 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ src/javax/persistence/GeneratorType.java
@@ -0,0 +1,8 @@
+//$Id$
+//EJB3 Specification Copyright 2004, 2005 Sun Microsystems, Inc.
+package javax.persistence;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public enum GeneratorType { TABLE, SEQUENCE, IDENTITY, AUTO, NONE };
