dce37e59a8f5a464116a0cd979cebcd83bdd268e
Steve Ebersole <steve.ebersole@jboss.com>
2009-07-17 16:54:42 +0000

    getSelectionItems() supposedly going away
    git-svn-id: http://anonsvn.jboss.org/repos/hibernate/jpa-api/trunk@17135 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/main/java/javax/persistence/criteria/CompoundSelection.java
+++ src/main/java/javax/persistence/criteria/CompoundSelection.java
@@ -2,19 +2,10 @@
 // EJB3 Specification Copyright 2004-2009 Sun Microsystems, Inc.
 package javax.persistence.criteria;
 
-import java.util.List;
-
 /**
  * The CompoundSelection interface defines compound selection item
  * (tuple, array, or result of constructor).
  * @param <X> the type of the selection item
  */
 public interface CompoundSelection<X> extends Selection<X> {
-
-    /**
-     * Return a selection items that were composed to form
-     * the compound selection.
-     * @return list of selection items
-     */
-    List<Selection<?>> getSelectionItems();
 }
