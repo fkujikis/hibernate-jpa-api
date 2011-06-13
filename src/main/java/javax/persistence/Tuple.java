2124279f49a5ff6ea79b34a7742e813fd3d0ca24
Strong Liu <stliu@hibernate.org>
2011-06-13 09:58:51 +0000

    update licenses
    git-svn-id: http://anonsvn.jboss.org/repos/hibernate/jpa-api/trunk@20957 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/main/java/javax/persistence/Tuple.java
+++ src/main/java/javax/persistence/Tuple.java
@@ -1,5 +1,20 @@
+/*
+ * Copyright (c) 2008, 2009 Sun Microsystems. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 and Eclipse Distribution License v. 1.0
+ * which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *     Linda DeMichiel - Java Persistence 2.0 - Version 2.0 (October 1, 2009)
+ *     Specification available from http://jcp.org/en/jsr/detail?id=317
+ */
+
 // $Id: $
-// EJB3 Specification Copyright 2004-2009 Sun Microsystems, Inc.
+
 package javax.persistence;
 
 import java.util.List;
